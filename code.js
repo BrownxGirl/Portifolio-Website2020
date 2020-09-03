b172b432dac547edb1185a1075bb53570af80456
Nomanna Oprah Mavata <nomannaoprah@gmail.com>
2020-09-03 03:33:09 +0200

    redesign of website, email form now functional

---
+++ code.js
@@ -0,0 +1,17 @@
+initialize class bankAcc
+constructor properties (name, id, address, pin, acctype, accCreatedDate)
+
+savingsAcc(){
+userobject = new bankAcc(
+this.name
+this.id
+this.address
+this.acctype
+this.pin
+)
+
+fs.writeFile(
+if accholders info is saved, then print acc created
+)
+}
+group 3
